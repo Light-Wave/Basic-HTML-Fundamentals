94b9bae2869fa1b0bb3dbfe0f9ebb2fcdb307ca4
Light-Wave <66904273+Light-Wave@users.noreply.github.com>
2025-09-01 19:34:49 +0200

    Friday

--- prime_square/prime_square.js
+++ prime_square/prime_square.js
@@ -1,18 +1,21 @@
 "use strict";
-const mapSize = 109;
+const mapSize = 309;
 const map = [];
 let primes = [];
 
 function CalcPrimest() {
   for (let i = 2; i < mapSize * mapSize; i++) {
     let sqrt = Math.sqrt(i);
+    let mightBePrime = true;
     for (let j = 0; j < primes.length && primes[j] <= sqrt; j++) {
       if ((i / primes[j]) % 1.0 == 0.0) {
-        CalcNext();
-        return;
+        mightBePrime = false;
+        break;
       }
     }
-    primes.push(i);
+    if (mightBePrime) {
+      primes.push(i);
+    }
   }
 }
 function GenerateMap() {
@@ -45,13 +48,38 @@ function GenerateMap() {
 function PaintCells() {
   let pos = [Math.floor(mapSize / 2), Math.floor(mapSize / 2)];
   let directions = [
-    [0, 1],
     [1, 0],
-    [-1, 0],
     [0, -1],
+    [-1, 0],
+    [0, 1],
   ];
   let curentDir = 0;
   let sideLength = 1;
+  let remainingSteps = 1;
+  let nextPrime = 0;
+  let i = 1;
+  while (pos[0] >= 0 && pos[0] < mapSize && pos[1] >= 0 && pos[1] < mapSize) {
+    let cell = map[pos[0]][pos[1]];
+    if (primes[nextPrime] == i) {
+      nextPrime++;
+      cell.style.backgroundColor = "white";
+    } else {
+      cell.style.backgroundColor = "black";
+    }
+    pos[0] += directions[curentDir][0];
+    pos[1] += directions[curentDir][1];
+    remainingSteps--;
+    if (remainingSteps == 0) {
+      curentDir = (curentDir + 1) % 4;
+      if (curentDir == 0 || curentDir == 2) {
+        sideLength++;
+      }
+      remainingSteps = sideLength;
+    }
+    i++;
+    //await new Promise((r) => setTimeout(r, 16)); // Useful for debugging
+  }
 }
 GenerateMap();
 CalcPrimest();
+PaintCells();
